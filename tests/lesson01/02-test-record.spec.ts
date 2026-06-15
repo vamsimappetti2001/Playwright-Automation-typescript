531b14f9e0e5f7a8644e2412e835ea8a2c3db5a9
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-06-15 19:25:30 +0530

    refactor test case in 02-test-record.spec.ts: update validation to check visibility of tutorial link

--- tests/lesson01/02-test-record.spec.ts
+++ tests/lesson01/02-test-record.spec.ts
@@ -12,7 +12,14 @@ test('My First Playwright TypeScript Test', async ({page}) => {
     await page.getByRole('combobox', { name: 'Search' }).press('Enter');
 // click on the playlists
     await page.getByRole('link', { name: 'Playwright by Testers Talk' }).first().click()
+    await page.waitForLoadState('networkidle');
 
 //Validate web page title
-    await expect(page).toHaveTitle('Playwright by Testers Talk - YouTube');
+   await expect(page.getByRole('link', { name: '#1 Playwright Tutorial Full' })).toBeVisible();
 });
+
+
+
+
+
+//await expect(page).toHaveTitle('Playwright by Testers Talk - YouTube');
