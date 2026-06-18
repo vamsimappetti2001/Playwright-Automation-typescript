fe77820ef1e9c338ce17edc70aac520284b78640
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-06-18 20:15:36 +0530

    fix(tests): uncomment mouse click actions in mouse actions test case

--- tests/Chapter02/06_Mouse_Actions_Test.spec.ts
+++ tests/Chapter02/06_Mouse_Actions_Test.spec.ts
@@ -1,19 +1,22 @@
 ﻿// import playwright module
 import {test, expect} from '@playwright/test'
 //write a test case
-test('Handiling Iframes,Drag and Drop element',  async ({ page}) =>{
+test('Mouse Actions In Playwright',  async ({ page}) =>{
 // go to url
 await page.goto('https://www.google.com/search?q=playwright+by+testers+talk'); 
 
 // left button click
-//await page.getByRole('link',{name :'Playwright by Testers Talk'}).first().click({ button :'left' })
+await page.getByRole('link',{name :'Playwright by Testers Talk'}).first().click({ button :'left' })
 
 // middle button click
-//await page.getByRole('link',{name :'Playwright by Testers Talk'}).first().click({ button :'middle' })
+await page.getByRole('link',{name :'Playwright by Testers Talk'}).first().click({ button :'middle' })
 
 // Right button click
 await page.getByRole('link',{name :'Playwright by Testers Talk'}).first().click({ button :'right' })
 
- 
+// check  Mouse Hover is working or not 
+await page.getByLabel('Search by voice').hover();
 
+//Doubleclick on the Element
+await page.getByLabel('Search by voice').dblclick ();
 });
