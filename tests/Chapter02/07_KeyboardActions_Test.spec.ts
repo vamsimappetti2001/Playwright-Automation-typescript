ff7aceb7c3066a8041545e40de76b357013547d6
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-06-18 22:03:24 +0530

    feat(tests): add keyboard actions test case for Google search functionality

---
+++ tests/Chapter02/07_KeyboardActions_Test.spec.ts
@@ -0,0 +1,25 @@
+// import playwright module
+import {test, expect } from '@playwright/test'
+
+//write a test case
+test('Keyboard actions in playwright', async({page}) =>{ 
+
+// go to url
+await page.goto('https://www.google.com/');
+
+//eneter action from keyboard
+ await page.getByLabel('Search', {exact :true}).first().click();
+await page.getByLabel('Search', {exact :true}).first().fill('Playwright by tester talk');
+await page.getByLabel('Search', {exact :true}).first().press('Enter');
+
+// selecting & deleting  from keyboard
+await page.getByLabel('Search', {exact :true}).first().click();
+await page.keyboard.press('Control+A');
+await page.keyboard.press('Delete');
+
+// Press and Enter
+await page.getByLabel('Search',  {exact :true}).first().click();
+await page.keyboard.press('Tab');
+await page.keyboard.press('Enter');
+
+});
