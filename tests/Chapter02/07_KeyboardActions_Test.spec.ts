2eb67cded8fd6048e2f924b9c509772ea648e785
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-06-18 22:04:25 +0530

    fix(tests): correct indentation in keyboard actions test case

--- tests/Chapter02/07_KeyboardActions_Test.spec.ts
+++ tests/Chapter02/07_KeyboardActions_Test.spec.ts
@@ -8,7 +8,7 @@ test('Keyboard actions in playwright', async({page}) =>{
 await page.goto('https://www.google.com/');
 
 //eneter action from keyboard
- await page.getByLabel('Search', {exact :true}).first().click();
+await page.getByLabel('Search', {exact :true}).first().click();
 await page.getByLabel('Search', {exact :true}).first().fill('Playwright by tester talk');
 await page.getByLabel('Search', {exact :true}).first().press('Enter');
 
