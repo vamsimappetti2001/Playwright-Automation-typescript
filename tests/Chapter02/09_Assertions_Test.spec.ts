954c0ab2b1747f97bb07a13562c815b8f7594d5b
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-06-19 20:33:58 +0530

    fix(tests): correct spacing in assertions for YouTube search functionality

--- tests/Chapter02/09_Assertions_Test.spec.ts
+++ tests/Chapter02/09_Assertions_Test.spec.ts
@@ -11,7 +11,7 @@ test('Assertions in Playwright', async({page}) =>{
     await expect(page.getByPlaceholder('Search', { exact : true }).first()).toBeVisible();
     await expect(page.getByPlaceholder('Search', { exact :true}).first()).toBeEditable();
     await expect(page.getByPlaceholder('Search', { exact : true}).first()).toBeEnabled();
-    await expect(page.getByPlaceholder('Search', {exact : true}).first()).toBeEmpty();
+    await expect(page.getByPlaceholder('Search', { exact : true}).first()).toBeEmpty();
 
     // Verify URL, Page Tittle, Text, Count
      
