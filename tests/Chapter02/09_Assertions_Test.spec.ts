0ec7e4d5b0fbbfbc6105227ac5a08c168fb31d1d
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-06-19 20:33:38 +0530

    feat(tests): add assertions test case for YouTube search functionality

---
+++ tests/Chapter02/09_Assertions_Test.spec.ts
@@ -0,0 +1,19 @@
+//import playwright module
+import{test, expect} from '@playwright/test'
+
+//write a test case 
+test('Assertions in Playwright', async({page}) =>{
+    
+    // Go to url
+    await page.goto('https://www.youtube.com/');
+
+    // Visible, editable, enabled, empty
+    await expect(page.getByPlaceholder('Search', { exact : true }).first()).toBeVisible();
+    await expect(page.getByPlaceholder('Search', { exact :true}).first()).toBeEditable();
+    await expect(page.getByPlaceholder('Search', { exact : true}).first()).toBeEnabled();
+    await expect(page.getByPlaceholder('Search', {exact : true}).first()).toBeEmpty();
+
+    // Verify URL, Page Tittle, Text, Count
+     
+         
+});
