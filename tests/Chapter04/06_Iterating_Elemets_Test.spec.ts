6d299cddd6641cf3d5ec6e1c913c3ecb93d0a1a8
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-08-06 20:59:45 +0530

    fix(tests): correct URL in repository elements iteration test

--- tests/Chapter04/06_Iterating_Elemets_Test.spec.ts
+++ tests/Chapter04/06_Iterating_Elemets_Test.spec.ts
@@ -4,7 +4,7 @@ import { test } from '@playwright/test'
 // write a test code
 test('print matching repository elements in playwright', async ({ page }) => {
     // go to url
-    await page.goto('https://github.com/vamsimappetti20    01');
+    await page.goto('https://github.com/vamsimappetti2001');
     await page.waitForSelector('main a.min-width-0.Link.text-bold.flex-auto.wb-break-all');
 
     const repositoryLinks = await page.$$('main a.min-width-0.Link.text-bold.flex-auto.wb-break-all');
