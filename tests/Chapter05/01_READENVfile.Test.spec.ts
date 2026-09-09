edd049ae7e8101e9a99a3b9f38ce778ac180a102
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-09-09 16:25:43 +0530

    Add new Playwright tests and update dependencies
    - Created multiple new test result files for various Playwright tests, including mouse actions, browser context, and parameterized tests.
    - Added new test files for reading environment variables and a seed test.
    - Updated package.json to include dotenv as a dependency for environment variable management.
    - Modified playwright.config.js to load environment variables from a .env file.
    - Added README.md in the specs directory for test plans documentation.
    - Updated package-lock.json to reflect the addition of dotenv and updated dependencies.

---
+++ tests/Chapter05/01_READENVfile.Test.spec.ts
@@ -0,0 +1,20 @@
+// import playwright module
+import {test, expect} from '@playwright/test'
+
+// write a test 
+test('My First Playwright TypeScript Test', async ({page}) => { 
+    
+// go to url
+    await page.goto(`${process.env.GOOGLE_URL}`);
+
+// search for a keywords
+    await page.getByRole('combobox', { name: 'Search' }).fill('playwright by testers talk');
+    await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+
+// click on the playlists
+    await page.getByRole('link', { name: 'Playwright by Testers Talk' }).first().click()
+   
+//Validate web page title
+   await expect(page.getByRole('link', { name: '#1 Playwright Tutorial Full' })).toBeVisible();
+
+});
