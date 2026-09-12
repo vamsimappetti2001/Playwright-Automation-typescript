bd5a4667b6bd5cc4d8c471a3ec039d767ddd2b8b
vamsimappetti2001 <vamsimappetti@gmail.com>
2026-09-12 13:11:32 +0530

    refactor(tests): update Test 3 to navigate to YouTube and validate URL

--- tests/Chapter03/01_Annotations_Test.spec.ts
+++ tests/Chapter03/01_Annotations_Test.spec.ts
@@ -44,16 +44,8 @@ test.only('Test 3', async ({page}) => {
     console.log('Test execution started...')
 
 // go to url
-    await page.goto('https://www.google.com/');
-
-// search for a keywords
-    await page.getByRole('combobox', { name: 'Search' }).fill('playwright by testers talk');
-    await page.getByRole('combobox', { name: 'Search' }).press('Enter');
-
-// click on the playlists
-    await page.getByRole('link', { name: 'Playwright by Testers Talk' }).first().click()
-    //await page.waitForLoadState('networkidle');
+    await page.goto('https://www.youtube.com/@testerstalk');
 
 //Validate web page title
-   await expect(page.getByRole('link', { name: '#1 Playwright Tutorial Full' })).toBeVisible();
+   await expect(page).toHaveURL(/youtube\.com\/\@testerstalk/);
 });
